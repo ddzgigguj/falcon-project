c3051aeab4641024f270baeb71d7fa5d77641033
Hanter <73187954+ddzgigguj@users.noreply.github.com>
2026-06-05 03:01:17 +0800

    Add accounts/urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.user_dashboard, name='user_dashboard'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+]
